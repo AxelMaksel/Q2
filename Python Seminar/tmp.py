cdf35ead3ac84ca4266fdf9a6d1312a597a393ca
Aleksey <88310719+AxelMaksel@users.noreply.github.com>
2022-12-15 23:20:51 +0700

    Revert "Home work 1-5"

--- Python Seminar/tmp.py
+++ Python Seminar/tmp.py
@@ -1,15 +1,8 @@
-# print("qwe")
-#
-# m = 0
-# for i in range(5):
-#     n = int(input("Введите число "))
-#     if n > m:
-#         m = n
-# print(f"max {m}")
+print("qwe")
 
-x = 1
-w = 1
-if !x == !w:
-    print("QW")
-else:
-    print("NO")
+m = 0
+for i in range(5):
+    n = int(input("Введите число "))
+    if n > m:
+        m = n
+print(f"max {m}")
