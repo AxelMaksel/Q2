fdca1b79d24ea9045d671bb4a2d1b0cc2d8d807b
AxelMaksel <am3mail@gmail.com>
2022-12-15 22:17:57 +0700

    HW1_3 ok

--- Python Seminar/HW1_2.py
+++ Python Seminar/HW1_2.py
@@ -8,5 +8,5 @@ for w in range(2):
     for z in range(2):
         for y in range(2):
             for x in range(2):
-                if not (w and z) or not y or (not x == not w):
+                if not (w and z) or not y or (not x != w):
                     print(w, z, y, x)
